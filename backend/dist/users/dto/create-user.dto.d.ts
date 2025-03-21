fbbe4cf64c93ee5de4e6fdfac7bef59237d2cc4d
MrWisz <eduardoluis09@hotmail.com>
2025-03-21 12:39:53 -0400

    front de jennifer

---
+++ backend/dist/users/dto/create-user.dto.d.ts
@@ -0,0 +1,17 @@
+export declare class CreateUserDto {
+    readonly nombre: string;
+    readonly email: string;
+    readonly telefono: string;
+    readonly direccion: {
+        calle: string;
+        ciudad: string;
+        pais: string;
+    };
+    readonly gustos: string[];
+    readonly foto_perfil: string;
+    readonly password_hash: string;
+    readonly favoritos: string[];
+    readonly seguidores: string[];
+    readonly seguidos: string[];
+    readonly fecha_registro: Date;
+}
---
+++ backend/dist/users/users.controller.d.ts
@@ -0,0 +1,9 @@
+import { UsersService } from './users.service';
+import { User } from './schemas/user.schema';
+import { CreateUserDto } from './dto/create-user.dto';
+export declare class UsersController {
+    private readonly usersService;
+    constructor(usersService: UsersService);
+    create(createUserDto: CreateUserDto): Promise<User>;
+    findAll(): Promise<User[]>;
+}
