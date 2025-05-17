c464eac794ea9eec54a2e2ec99d60a1a934749e9
yetzimontilla <1001.27250497.ucla@gmail.com>
2025-05-17 18:54:51 -0400

    se hizo correcciones en el endpoint de likes

--- backend/src/likes/likes.service.ts
+++ backend/src/likes/likes.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
-import { Like } from './schema/like.schema';
+import { Like } from './schemas/like.schema';
 
 @Injectable()
 export class LikesService {
