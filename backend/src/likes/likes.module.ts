250202fb86bb1ade5a908b705fbdec01e278fd58
MrWisz <eduardoluis09@hotmail.com>
2025-05-17 21:51:15 -0400

    añadiendo actualización del like en la colección de posts

--- backend/src/likes/likes.module.ts
+++ backend/src/likes/likes.module.ts
@@ -3,10 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { LikesService } from './likes.service';
 import { LikesController } from './likes.controller';
 import { Like, LikeSchema } from './schemas/like.schema';
+import { Post, PostSchema } from '../posts/schemas/post.schema'; 
+
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Like.name, schema: LikeSchema }]),
+    MongooseModule.forFeature([{ name: Like.name, schema: LikeSchema },
+      { name: Post.name, schema: PostSchema },]),
   ],
   controllers: [LikesController],
   providers: [LikesService],
