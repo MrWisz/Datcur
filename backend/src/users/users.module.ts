fe19631a1d1682a249af986453cf057586db90d3
MrWisz <eduardoluis09@hotmail.com>
2025-05-24 23:14:30 -0400

    Perfil de otros usuarios conectado

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,14 +1,17 @@
-// filepath: c:\Users\Luis\Desktop\Proyectos\Datcur\backend\src\users\users.module.ts
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User, UserSchema } from './schemas/user.schema';
+import { Post, PostSchema } from '../posts/schemas/post.schema'; 
 import { CloudinaryModule } from '../cloudinary/cloudinary.module';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Post.name, schema: PostSchema }, 
+    ]),
     CloudinaryModule,
   ],
   providers: [UsersService],
